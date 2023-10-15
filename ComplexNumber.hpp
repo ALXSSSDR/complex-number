d26614f6ccbcbc3197686bced20b21f9b19f6ce7
Akexandr Zhukov <147894134+ALXSSSDR@users.noreply.github.com>
2023-10-15 22:56:07 +1000

    Add files via upload

---
+++ ComplexNumber.hpp
@@ -0,0 +1,45 @@
+#ifndef ComplexNumber_hpp
+#define ComplexNumber_hpp
+
+#include <iostream>
+
+using namespace std;
+
+class ComplexNumber {
+private:
+    double real;            // Действительная часть
+    double imaginary;       // Мнимая часть
+    
+public:
+    ComplexNumber(double real, double imaginary) : real(real), imaginary(imaginary) {}
+    
+    double getReal() const;
+    
+    void setReal(double real);
+    
+    double getImaginary() const;
+    
+    void setImaginary(double imaginary);
+    
+    ComplexNumber operator+(const ComplexNumber& other) const;
+    
+    ComplexNumber operator-(const ComplexNumber& other) const;
+    
+    ComplexNumber operator*(const ComplexNumber& other) const;
+    
+    ComplexNumber operator/(const ComplexNumber& other) const;
+    
+    bool operator==(const ComplexNumber& other) const;
+    
+    bool operator!=(const ComplexNumber& other) const;
+    
+    bool operator==(double num);
+    
+    ComplexNumber operator^(int exp) const;
+    
+    double getMagnitude() const;
+    
+    void display();
+};
+
+#endif
